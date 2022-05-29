1186692fb6a1f2cffcf2060f8aced07ed43e7a64
Amit DebNath <amitdebnath@Amits-MacBook-Pro.local>
2022-05-29 20:28:42 +0600

    project  setup

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#6246ea",
+
+          secondary: "#d1d1e9",
+
+          accent: "#6246ea",
+
+          neutral: "#1D283A",
+
+          "base-100": "#fffffe",
+
+          info: "#0CA6E9",
+
+          success: "#86efac",
+
+          warning: "#fde68a",
+
+          error: "#ef4444",
+        },
+      },
+      "dark",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
